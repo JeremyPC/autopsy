d3abf8d88aeba48730b041243a792a95aa330c99
JeremyPC <jeremypcox@gmail.com>
2016-12-11 23:25:49 +1100

    MAVLink test code added.

--- nodefiles/my_web_server.js
+++ nodefiles/my_web_server.js
@@ -1,14 +1,9 @@
 var express = require("express");
 var app = express();
 
-console.log("requires complete.");
 
 app.get("/",function(req,res){
         res.send("<h1>Hello from EC2</h1>");
 });
 
-console.log("get complete.");
-
 app.listen(80);
-
-console.log("listen complete.");
